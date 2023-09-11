0a12f96399d3f3f0ed3eeb805cba25d8de5a8fde
Przemek8686 <1986krawiec1986@gmail.com>
2023-09-11 21:48:04 +0200

    css correct

--- src/comps/Tiitle.js
+++ src/comps/Tiitle.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export const Title = () => {
   return (
     <div className="title">
       <h1>OLIMPIC EKIPA</h1>
       <h2>"Kto znalazł przyjaciela, skarb znalazł"</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-  )
+  );
 };
-
